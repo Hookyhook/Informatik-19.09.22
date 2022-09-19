833a338e1524e3b51904126240f7a0043025bf99
Hookyhook <108137850+Hookyhook@users.noreply.github.com>
2022-09-19 15:38:55 +0200

    h

--- TAXI.java
+++ TAXI.java
@@ -1,18 +1,18 @@
 class TAXI {
     // Attribute
-    // Ergänzen Sie die fehlenden Attribute, s.Klassenkarte S.11
+    // Ergï¿½nzen Sie die fehlenden Attribute, s.Klassenkarte S.11
   
     // Konstrutktor
     public TAXI(String k, String f) {
-       // Ergänzen Sie den Konstruktor der Klasse Taxi 
+       // Ergï¿½nzen Sie den Konstruktor der Klasse Taxi 
        //  = k; // die Eingabeparamter k und f werden an die
-       // = f;  // Attribute übergeben
+       // = f;  // Attribute ï¿½bergeben
        
     }
     
     // Methoden
     public String kennzeichenGeben(){
-        // hier fehlt was
+        // hier fehlt wass
     }
       
     public String  fahrernameGeben(){
