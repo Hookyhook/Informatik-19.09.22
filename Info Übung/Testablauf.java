635d83e474e2fac7d8e54d71a4dd337a0e8156a4
Hookyhook <108137850+Hookyhook@users.noreply.github.com>
2022-09-21 19:07:48 +0200

    s

--- Info Übung/Testablauf.java
+++ Info Übung/Testablauf.java
@@ -1,20 +1,29 @@
 class TESTABLAUF {
-    // Ergänzen Sie den passenden Namen
+    // Ergï¿½nzen Sie den passenden Namen
     TAXI taxiA,taxiB,taxiC,taxiD,taxiE,taxiF,taxiG,taxiH,taxiI,taxiJ;
     
     TAXIWARTESCHLANGE taxiwarteschlange1=new TAXIWARTESCHLANGE();
    
-    // Ergänzen Sie den passenden Namen
-    public TESTABLAUF() {
-        
+    // Ergï¿½nzen Sie den passenden Namen
+    public static void main(String[]args) {
+       ablaufen(); 
     }
     
-    // Ergänzen Sie den passenden Namen
+    // Ergï¿½nzen Sie den passenden Namen
     private void taxisErzeugen(){
         // hier sollen 10 Taxis erzeugt werden, TaxiA...TaxiJ 
         // mit den Fahrern Britta, Hans, Peter, Willi, Hubert, Paula, Eva, Otto, Iris, Norbert
         // Jedes Taxi hat ein Kennzeichen und einen Fahrernamen
-        
+        taxiA = new TAXI("M 7343", "Britta");
+        taxiB = new TAXI("M 74543", "Hans");
+        taxiC = new TAXI("M 73543", "Peter");
+        taxiD = new TAXI("M 73453", "Willi");
+        taxiE = new TAXI("M 7345", "Hubert");
+        taxiF = new TAXI("M 7123", "Paula");
+        taxiG = new TAXI("M 72345", "Eva");
+        taxiH = new TAXI("M 72345", "Otto");
+        taxiI = new TAXI("M 7456", "Iris");
+        taxiJ = new TAXI("M 74645", "Norbert");
     }
     public void ablaufen(){
         taxisErzeugen();
